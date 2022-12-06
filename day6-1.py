cb0d6bc0e7e24a8bae441bdddaf915e893056ca2
Bill Wu <bill.wu@bjss.com>
2022-12-06 07:30:32 +0000

    Day 6

---
+++ day6-1.py
@@ -0,0 +1,21 @@
+from aoc_lib import fileToArrayNoStrip
+
+day6input = fileToArrayNoStrip("day6-1.txt")
+
+def find_unique_characters(input, target):
+    head=0
+    for index in range(len(input)):
+        for i in range(head,index):
+            if input[i]==input[index]:
+                head=i+1
+                break
+        if index-head==target-1:
+            return index+1
+    return False
+
+for i in day6input:
+    if i=="":
+        break
+    else:
+        print(find_unique_characters(i,4))
+        print(find_unique_characters(i,14))
